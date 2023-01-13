79d1af46d709f61afd06d54b23865eca51666da8
Natalia Derevianko <derevyanko.n7@gmail.com>
2023-01-13 18:42:33 +0100

    romove active

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
-"use strict";
+'use strict';
 
 const titleClickHandler = function (event) {
-  console.log("Links was clicked");
-  console.log(event);
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Links was clicked');
+
+  /* [DONE] remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+
+  /* [DONE] add class 'active' to the clicked link */
+
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post');
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
+  /* [DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('właściwy artykuł: ', targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 };
 
-/* remove class 'active' from all article links  */
-
-/* add class 'active' to the clicked link */
-
-/* remove class 'active' from all articles */
-
-/* get 'href' attribute from the clicked link */
-
-/* find the correct article using the selector (value of 'href' attribute) */
-
-/* add class 'active' to the correct article */
-
-const links = document.querySelectorAll(".titles a");
-
+const links = document.querySelectorAll('.titles a');
 for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
